526e30df704a2c594277e2982e363e7f13976d52
Drewthyson <gustavo.lacerda627@gmail.com>
2024-10-18 18:20:14 -0300

    Added some features and toggle color scheme to light/dark

--- EcommerceFront/src/App.jsx
+++ EcommerceFront/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from '@mantine/core';
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button, useMantineColorScheme, Group } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import "./app.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { toggleColorScheme } = useMantineColorScheme();
+  const [opened, { toggle }] = useDisclosure(false);
 
   return (
     <>
+      <Group>
+        <Button onClick={() => toggleColorScheme()}>
+          Modo Claro ou escuro
+        </Button>
+      </Group>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,9 +23,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
-        <Button variant="filled" color="blue" size="xl" radius="xl">Button</Button>
+        <Button variant="filled" color="purple" size="xl" radius="xl">
+          Button
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -27,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- EcommerceFront/src/main.jsx
+++ EcommerceFront/src/main.jsx
@@ -1,26 +1,53 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { react, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { createTheme, MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider } from "@mantine/core";
 import '@mantine/core/styles.css';
-import App from './App.jsx'
+import App from "./App.jsx";
+import './index.css'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <App />,
   },
 ]);
 
 const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    // Add your color
+    purple: [
+      "#faedff",
+      "#edd9f7",
+      "#d8b1ea",
+      "#c186dd",
+      "#ae62d2",
+      "#a34bcb",
+      "#9d3fc9",
+      "#8931b2",
+      "#7a2aa0",
+      "#6b218d"
+    ],
+  },
+
+  shadows: {
+    md: "1px 1px 3px rgba(0, 0, 0, .25)",
+    xl: "5px 5px 3px rgba(0, 0, 0, .25)",
+  },
+
+  headings: {
+    fontFamily: "Roboto, sans-serif",
+  },
 });
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <MantineProvider theme={theme}>
-      <StrictMode>
-        <RouterProvider router={router} />
-      </StrictMode>
-    </MantineProvider>
-)
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </MantineProvider>
+);
